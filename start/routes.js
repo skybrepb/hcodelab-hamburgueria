220bf125b904bdf21e65cd0acbed7074fe26a47d
Evandro Pereira <evandro@optiplex.com.br>
2020-06-11 13:53:52 -0300

    Inclusão Rota Contato

--- start/routes.js
+++ start/routes.js
@@ -25,9 +25,14 @@ Route.post('/faleconosco', 'FaleconoscoController.store')
 Route.post('/cardapio', 'CardapioController.store')         // Inclui um registro
 Route.get('/cardapio', 'CardapioController.index')          // lista todos os registros
 Route.get('/cardapio/:id', 'CardapioController.show')       //Lista um registro especifico
-Route.post('/cardapio/:id', 'CardapioController.update')    // Atualiza um registro
+Route.put('/cardapio/:id', 'CardapioController.update')    // Atualiza um registro
 Route.delete('/cardapio/:id', 'CardapioController.destroy') // Deleta um registro
 
+Route.post('/contato', 'ContatoController.store')         // Inclui um registro
+Route.get('/contato', 'ContatoController.index')          // lista todos os registros
+Route.get('/contato/:id', 'ContatoController.show')       //Lista um registro especifico
+Route.put('/contato/:id', 'ContatoController.update')    // Atualiza um registro
+Route.delete('/contato/:id', 'ContatoController.destroy') // Deleta um registro
 
 
 
