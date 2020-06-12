09bc6d829dabeb88637b0c0eae6164f536bb6bf1
Evandro Pereira <evandro@optiplex.com.br>
2020-06-12 00:32:37 -0300

    Admin interface2

--- app/Controllers/Http/ContatoController.js
+++ app/Controllers/Http/ContatoController.js
@@ -12,6 +12,40 @@ class ContatoController {
     return contato
   }
 
+  async index () {    // Lista todos os usuários
+    return await Contato.all()
+  }
+
+  async show({ params }) {    // Lista um usuário pelo ID
+    const contato = await Contato.findOrFail(params.id)
+
+    return contato
+  }
+
+  async destroy({ params, auth, response}){
+      const contato = await Contato.findOrFail(params.id)
+
+      if(contato.id !== auth.contato.id) {
+          return response.status(401).send({ error: 'Not Autorized'})
+      }
+      await contato.delete()
+  }
+
+  async update ({ params, request, response}) {
+      // Efetua a busca do registro na tabela
+      const contato = await Contato.findOrFail(params.id)
+      //Preparando alteração
+      const data = request.only(['name','email','phone','birth_at'])
+
+      contato.merge(data)
+      // Executa o commit na tabela
+      await contato.save()
+
+      return contato
+  }
+
+
+
 }
 
 module.exports = ContatoController
